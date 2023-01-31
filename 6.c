14a7bca9d81bad9a4a035a72da3f08731616a48c
chaitanya1654 <119470685+chaitanya1654@users.noreply.github.com>
2023-01-31 21:10:51 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Descendant Combinator</title>
+    <link rel="stylesheet" href="style5.css">
+  </head>
+  <body>
+    <div class="container">
+      <p class="text">Some of the key practical benefits of innovation and creativity are:</p>
+    </div>
+    <p class="text">improved productivity.</p>
+    <p class="text">increased competitiveness.</p>
+    <div class="container">
+      <p class="text">improved brand recognition and value.</p>
+      <p class="text">improved brand recognition and value.</p>
+    </div>
+    <p> <center>
+     <a href = "https://spoorthytorne.github.io/css-exercises/problem05.html">Next</a>
+      </center></p>
+  </body>
+</html>
